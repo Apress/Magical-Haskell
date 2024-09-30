edf21b89d96fb849ff66a0cc6e2d4e78c74f4aec
Anton Antich <aantich@gmail.com>
2024-09-30 19:23:25 +0200

    in progress

--- ch09/app/Main.hs
+++ ch09/app/Main.hs
@@ -1,7 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
+
 import Init (initConfig)
+import StackTypes (findProviderByName)
+
+import Network.HTTP.Client (Manager, newManager)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import LLM.OpenAI (chatCompletion, userMessage, testChunkStreaming)
+
 
 main :: IO ()
-main = do 
+main = do
     settings <- initConfig
-    print settings
+    let prov = findProviderByName settings "openai"
+    print prov
+    manager <- newManager tlsManagerSettings
+    chatCompletion manager [userMessage "hi"] "gpt-4o" prov Nothing testChunkStreaming
